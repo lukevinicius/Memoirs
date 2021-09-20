6aaf2d0445292774e64497cf66f436abfb9ae5d3
lukevinicius <lucasviniciusaalves@gmail.com>
2021-09-20 19:57:12 -0300

    add: Refresh Posts Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback, useEffect } from 'react'
-import { FlatList } from 'react-native';
+import { ScrollView, FlatList, RefreshControl, ToastAndroid } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { parseISO, format } from 'date-fns';
 
@@ -33,9 +33,29 @@ interface JournalProps {
 }
 
 export function Home() {
+  const [refreshing, setRefreshing] = useState(false);
   const navigation = useNavigation();
   const { user, signOut } = useAuth();
   const [data, setData] = useState<Journal[]>();
+  const [email, setEmail] = useState(true)
+
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+      try {
+        await api.get(`posts/${user.id}`)
+        .then((res) => {
+          setData(res.data);
+        })
+        .catch((err) => {
+          if (err.response.status === 403) {
+            signOut()
+          }
+        })
+        setRefreshing(false)
+      } catch (error) {
+        console.error(error);
+      }
+    }, [refreshing]);
 
   async function findPosts() {
     await api.get(`posts/${user.id}`)
@@ -51,13 +71,14 @@ export function Home() {
 
   useEffect(() => {
     findPosts()
-    return () => {
-      setData([]); // This worked for me
-    };
   }, []);
 
   return (
     <>
+    <ScrollView
+      contentContainerStyle={{flex: 1}}
+      refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+    >
       <Container>
         {/* <View>
           <Title>Your Journal</Title>
@@ -74,43 +95,42 @@ export function Home() {
         </View> */}
         <View>
           <ViewPost>
-            <Title>Posts Recentes</Title>
-            <Button onPress={() => navigation.navigate('AppStack', { screen: 'AddPost' })}>
+            <Title>Suas Memorias</Title>
+            <Button onPress={
+              () => email ? navigation.navigate('AppStack', { screen: 'AddPost' })
+              : navigation.navigate('AppStack', { screen: 'ConfirmEmail' })
+              }>
               <Text>Novo  +</Text>
             </Button>
           </ViewPost>
             {data === null ?
-            <FlatList
-              data={data}
-              keyExtractor={item => item._id}
-              renderItem={({item}) => (
-                <Content onPress={() =>
-                  navigation.navigate('AppStack', {
-                    screen: 'PostDetails',
-                    params: {postId: item.postId},
-                  })
-                }>
-                  {/* <Image source={imageTravel} /> */}
-                  <ViewJournal>
-                    <TitlePost>
-                      {format(parseISO(String(item.createdAt)), 'dd/MM/yyyy')}
-                    </TitlePost>
-                    <Description numberOfLines={1}>
-                      {item.body}
-                    </Description>
-                    <InfoPost>
-                      <InfoText style={{marginRight: 10}}>
-                        <Icon name="clock" size={12} color="#666360" />
-                        {format(parseISO(String(item.createdAt)), 'HH:mm')}
-                      </InfoText>
-                      {/* <InfoText>
-                        <Icon name="bookmark" size={12} color="#666360" />{item.category}
-                      </InfoText> */}
-                    </InfoPost>
-                  </ViewJournal>
-                </Content>
-              )}
-            />
+            data.map(item => (
+              <Content key={item._id} onPress={() =>
+                navigation.navigate('AppStack', {
+                  screen: 'PostDetails',
+                  params: {postId: item.postId},
+                })
+              }>
+                {/* <Image source={imageTravel} /> */}
+                <ViewJournal>
+                  <TitlePost>
+                    {format(parseISO(String(item.createdAt)), 'dd/MM/yyyy')}
+                  </TitlePost>
+                  <Description numberOfLines={1}>
+                    {item.body}
+                  </Description>
+                  <InfoPost>
+                    <InfoText style={{marginRight: 10}}>
+                      <Icon name="clock" size={12} color="#666360" />
+                      {format(parseISO(String(item.createdAt)), 'HH:mm')}
+                    </InfoText>
+                    {/* <InfoText>
+                      <Icon name="bookmark" size={12} color="#666360" />{item.category}
+                    </InfoText> */}
+                  </InfoPost>
+                </ViewJournal>
+              </Content>
+            ))
             : <ViewEmpty>
                 <TitlePost>Adicione uma Memoria</TitlePost>
                 <TitlePost>Ela vai aparecer aqui</TitlePost>
@@ -118,6 +138,7 @@ export function Home() {
           }
         </View>
       </Container>
+      </ScrollView>
     </>
   )
 }
